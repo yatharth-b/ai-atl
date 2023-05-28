47dce46df885a07a04f2d7673271567a02ba63e2
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-05-28 14:24:57 -0400

    next image

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import styles from './Hero.module.css';
+import Image from 'next/image';
 
 export default function Hero() {
   return (
     <div className={styles.hero_color}>
       <div className={styles.hero}>
         <div className={styles.hero_content_container}>
-          <img src='/logo.png' className={styles.logo}></img>
+          <Image src='/logo.png' width={300} height={208.5} priority={true}></Image>
           <h3 className={styles.hero_text}>
           Atlanta’s first Generative AI hackathon<br></br>
           hosted by <a href="https://startup.exchange/"><u>builders at Georgia Tech</u></a>
